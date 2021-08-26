7dba1892ef81e51f02ae1efcae22a32bc3c4aef0
David <david.parra@iaggbs.com>
2021-08-26 17:08:20 +0200

    Add a controller
    GET all action, GET by Id action

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+
+namespace ContosoPizza.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() =>
+            PizzaService.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if(pizza == null)
+                return NotFound();
+
+            return pizza;
+        }
+
+        // POST action
+
+        // PUT action
+
+        // DELETE action
+    }
+}
